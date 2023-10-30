6a652d26ec4b4b6b64cdba23a4d4eb7f9c9b8638
Muhammad Rizki <muhammad.rizki88@telkom.co.id>
2023-10-30 17:20:28 +0700

    create header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+import Header from './components/Header'
+import MenuItem from './components/MenuItem'
 import { Inter } from 'next/font/google'
 import './globals.css'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        <Header />
+        {/* { navbar } */}
+       
+        {/* { searchbox } */}
+
+        {children}
+        </body>
     </html>
   )
 }
