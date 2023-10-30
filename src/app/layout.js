42f199b460b2da11966db270dfd9c055e687aaca
Muhammad Rizki <muhammad.rizki88@telkom.co.id>
2023-10-30 19:57:22 +0700

    add darkmode

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,9 @@ import Header from './components/Header'
 import MenuItem from './components/MenuItem'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
-
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
   title: 'IMDB Clone',
   description: 'This is a clone of IMDB',
@@ -16,14 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>
+          <Header />
+          {/* { navbar } */}
         
-        <Header />
-        {/* { navbar } */}
-       
-        {/* { searchbox } */}
+          {/* { searchbox } */}
 
-        {children}
-        </body>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
